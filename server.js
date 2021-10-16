8fc4b1219cf3a5acf90f043d730ddb206d87951f
mmifs <85654339+mmifs@users.noreply.github.com>
2021-10-15 21:56:26 -0400

    post route works

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.post('/api/notes', (req, res) =>{
+    const newNote = req.body;
+
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    res.json(newNote);
+})
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
   });
+
+  //console.log(notes);
