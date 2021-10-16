e5d626d04bc30efd9724fdb47b1ade5b2ca86bdb
mmifs <85654339+mmifs@users.noreply.github.com>
2021-10-15 21:41:56 -0400

    fixed notes.html route

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
   });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './notes.html'));
   });
 
 app.get('/api/notes', (req, res) => {
@@ -22,5 +23,3 @@ app.get('/api/notes', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
   });
-
-  console.log(notes);
