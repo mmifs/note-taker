c3bb2f413e67f23e6f2b6b963d03c0130d25195a
mmifs <85654339+mmifs@users.noreply.github.com>
2021-11-02 13:42:53 -0400

    slight adjustments

--- server.js
+++ server.js
@@ -5,36 +5,46 @@ const app = express();
 const PORT = 3001;
 const fs = require('fs');
 const ds = require('fs');
+const { join } = require('path')
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(express.json());
 
-function postNewNote(body, notes) {
+function createNewNote(body, notesArray) {
     const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, '/db/db.json'),
+      JSON.stringify({notes: notesArray}, null, 2)
+    );
+    return note;
 };
 
+
+//get index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
   });
 
+
+  //get notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './notes.html'));
   });
 
+  //get notes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) =>{
-    const newNote = req.body;
-    
-
-    console.log(newNote);
-
-    notes.push(newNote);
-
-    res.json(newNote);
+  //req.body.id = notes.length+1
+  const note = createNewNote(req.body, notes);
+  //const newNote = req.body;
+  //console.log(newNote);
+  console.log(notes.length);
+  res.json(note);
 })
 
 app.listen(PORT, () => {
